1c92824eae7cc4ff9d627a4ab1d08bd0346c00f6
D€COD€R <59320957+mudirajesh@users.noreply.github.com>
2020-11-04 03:24:22 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     private fun loadMeme() {
         // Instantiate the RequestQueue.
         progressBar.visibility = View.VISIBLE
-        val url = ""
+        val  = ""
 
          // Request a string response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
